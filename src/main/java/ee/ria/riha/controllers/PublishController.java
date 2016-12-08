c0cae900d1e9f52009f8ac7c4c08c5b6b86219d0
Kirill Klenski, Kristo Kuiv <kirill@codeborne.com, kristo@codeborne.com>
2016-12-08 14:30:30 +0200

    Enable CORS for infosystems request

--- src/main/java/ee/ria/riha/controllers/PublishController.java
+++ src/main/java/ee/ria/riha/controllers/PublishController.java
@@ -3,6 +3,7 @@ package ee.ria.riha.controllers;
 import ee.ria.riha.service.InfosystemStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class PublishController {
 
   @Autowired InfosystemStorageService storageService;
 
+  @CrossOrigin
   @RequestMapping(value = "/infosystems/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
   public String infosystems() {
     return storageService.load();
